0318b304d90887311173152544e20e529e802a85
dev4kj <dev4kj@gmail.com>
2018-07-14 02:36:37 +0000

    17multiple2

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ function isMultipleOfSeventeen(num){
 }
 
 
+
 module.exports = {
     isMultipleOfSeventeen : isMultipleOfSeventeen
 }
+
+// if I put the comment on this File, can I make the history record on git WEB ?
